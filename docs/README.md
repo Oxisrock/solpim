17295665e9d4e72c539809ef7a36cf06ffc848ef
Caballero73 <comisionado.falcon@gmail.com>
2018-11-06 05:36:27 -0300

    Iniciando Repo

---
+++ docs/README.md
@@ -0,0 +1,19 @@
+# Enrique Valerio
+## Diseñador Gráfico
+
+### Página personal
+
+Diseñador Gráfico con 14 años de experiencia en diversas ramas del oficio. He trabajado principalmente como diseñador de Branding y Frontend Desing, ocupando puestos de supervisión en equipos creativos. En el área de impresión comercial, laboré como Jefe de Producción de una imprenta.
+
+Actualmente, luego de un proceso de crecimiento profesional dentro de esta área, me desempeño como Coordinador de Frontend Desing en Slice Group.
+
+Antes de unirme a Slice Group, empredí mi propia empresa de diseño gráfico donde tuve la suerte de trabajar con excelentes clientes, que me ayudaron a formar un criterio de calidad y disciplina. Posteriormente, en sociedad con empresarios jóvenes y de visión, fundamos Gráficas Litho Xpress, una compañía que de inmediato se destacó y al poco tiempo tomó el liderazgo como empresa de impresión comercial y publicitaria a nivel regional, allí laboré durante 12 años como Jefe de Producción, adquiriendo sólidos conocimientos en la materia, además de nociones claras sobre como dirigir un equipo de trabajo eficiente.
+
+Realmente soy un apasionado de mi trabajo, siempre con ganas de aprender, mientras disfruto de todos los aspectos de mi empleo, creo que mi etapa favorita en un proyecto es el desarrollo del mismo, trabajar con mis compañeros para cristalizar una idea, que luego sea realmente útil para nuestros clientes. A medida que avanzamos a través de ese proceso de colaboración, las ideas comienzan a fluir y eso siempre es la parte más edificadora.
+
+Aparte de mi formación académica tradicional, opté y quedé seleccionado para una Beca Empresarial de Platzi, donde he adquirido muchas herramientas útiles para mi actual desempeño profesional… Además, siempre estoy interesado en escuchar a compañeros, gerentes o simplemente gente interesante y creativa, que me permita mejorar cada día un poco más.
+
+### Equipo
+* [Enrique Valerio (Diseñador)](https://caballero73.github.io/enriquevalerio/ "Enrique Valerio - Diseñador")
+
+https://caballero73.github.io/enriquevalerio/
---
+++ gulpfile.js
@@ -0,0 +1,95 @@
+// Gulp *
+var gulp = require('gulp')
+// Uso de PostCSS *
+var postcss = require('gulp-postcss')
+// Importar archivos de CSS dentro de uno solo *
+var atImport = require ('postcss-import')
+// Extienden la sintaxis de CSS, la posibilidad de anidar clases *
+var cssnested = require('postcss-nested')
+// Para juntar media queries similares en una sola *
+var mqpacker = require('css-mqpacker')
+// Reutilizar estilos de CSS *
+var mixins = require('postcss-mixins')
+// Crear tamaños responsivos para las fuentes *
+var rucksack = require('rucksack-css')
+// Permite trabajar con parciales de HTML *
+var fileinclude = require('gulp-file-include')
+// Minificar CSS *
+var csswring = require('csswring')
+// Utilizar hoy la sintaxis CSS del mañana *
+var cssnext = require('postcss-cssnext')
+// Sincronizar navegador *
+var browserSync = require('browser-sync').create()
+
+// Servidor de desarrollo
+gulp.task('serve', function () {
+  browserSync.init({
+    server: {
+      baseDir: './docs'
+    }
+  })
+})
+
+// Tarea para procesar el CSS
+gulp.task('css', function () {
+  var processors = [
+    mixins(),
+    atImport(),
+    cssnested,
+    rucksack(),
+    cssnext({browsers:'last 5 versions'}),
+    // mqpacker,
+    //csswring()
+  ]
+  return gulp.src('./src/css/app.min.css')
+     .pipe(postcss(processors))
+     .pipe(gulp.dest('./docs/assets/css'))
+     .pipe(browserSync.stream())
+})
+
+// Tarea para procesar JS
+gulp.task('js', function() {
+  return gulp.src('./src/js/**/*.js') 
+    .pipe(gulp.dest('./docs/assets/js'))
+});
+
+// Tarea para procesar imágenes
+gulp.task('image', function() {
+   return gulp.src('./src/img/**/*.*') 
+   .pipe(gulp.dest('./docs/assets/img'))
+});
+
+// Tarea para procesar fuentes
+gulp.task('fonts', function() {
+   return gulp.src('./src/fonts/**/*.*') 
+   .pipe(gulp.dest('./docs/assets/fonts'))
+});
+
+// Tarea para procesar videos y otros archivos de medios
+gulp.task('medios', function() {
+   return gulp.src('./src/media/**/*.*') 
+   .pipe(gulp.dest('./docs/assets/media'))
+});
+
+// Tarea para utilizar fileinclude
+gulp.task('fileinclude', function() {
+  gulp.src('./src/*.html')
+    .pipe(fileinclude({
+      prefix: '@',
+      basepath: './src'
+    }))
+    .pipe(gulp.dest('./docs'));
+});
+
+// Tarea para vigilar los cambios
+gulp.task('watch', function () {
+  gulp.watch('./src/css/*.css', ['css']).on('change', browserSync.reload)
+  gulp.watch('./src/css/partials/*.css', ['css']).on('change', browserSync.reload)
+  gulp.watch('./src/js/**/*.js', ['js']).on('change', browserSync.reload)
+  gulp.watch('./src/img/**/*.*', ['image']).on('change', browserSync.reload)
+  gulp.watch('./src/fonts/**/*.*', ['fonts']).on('change', browserSync.reload)
+  gulp.watch('./src/media/**/*.*', ['medios']).on('change', browserSync.reload)
+  gulp.watch('./src/**/*.html', ['fileinclude']).on('change', browserSync.reload)
+});
+
+gulp.task('default', ['css', 'js', 'image', 'fonts', 'medios', 'fileinclude', 'watch', 'serve'])
